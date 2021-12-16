58caaf9c382e88337298e22afbad2c277a02121b
Ammar Abdilghanie <ama66@cornell.edu>
2021-12-16 13:35:48 -0600

    Update resampleInterp.py

--- resampleInterp.py
+++ resampleInterp.py
@@ -3,6 +3,13 @@
             newdate_range = pd.date_range(Min_Tstamp,Max_Tstamp, freq= sampling_interval) 
             df = df.reindex(self.newdaterange).interpolate(method='linear').resample(window_size).mean().bfill()
             
+            df['datetime_name']] = pd.to_datetime(df['datetime_name']]) 
+            df.set_index(df["datetime_name"],inplace=True)
+            
+        
+            df=df.rolling(window=rollwin).std()
+            
+            
             
        
    
